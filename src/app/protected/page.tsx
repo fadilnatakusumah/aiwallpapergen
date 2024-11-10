5c0b52d7b12ebb0018ddf78fbe6390aa7fc9b109
fadilnatakusumah <fadil.ntksmh@gmail.com>
2024-11-10 18:58:22 +0700

    fix: build

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,11 +1,10 @@
 // app/dashboard/layout.tsx
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import { type PropsWithChildren } from "react";
+// import { type PropsWithChildren } from "react";
 import { auth } from "~/server/auth";
-import { HydrateClient } from "~/trpc/server";
 
-export default async function DashboardLayout({ children }: PropsWithChildren) {
+export default async function Protected() {
   const session = await auth();
 
   // If there's no session, redirect to login
@@ -15,15 +14,14 @@ export default async function DashboardLayout({ children }: PropsWithChildren) {
 
   // Render the children (the page content)
   return (
-    <HydrateClient>
+    <div>
       <h1>Welcome to your dashboard, {session.user?.email}</h1>
-      <div>{children}</div>
       <Link
         href={session ? "/api/auth/signout" : "/api/auth/signin"}
         className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
       >
         {session ? "Sign out" : "Sign in"}
       </Link>
-    </HydrateClient>
+    </div>
   );
 }

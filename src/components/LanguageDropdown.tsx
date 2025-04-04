aac064d196596ed622ed3a610e1de5d3ebcc37fc
fadilnatakusumah <fadil.ntksmh@gmail.com>
2025-04-04 16:20:54 +0800

    feat: improve Country flag in chrome

--- src/components/LanguageDropdown.tsx
+++ src/components/LanguageDropdown.tsx
@@ -1,25 +1,27 @@
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
-import { Check, ChevronDown } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 import { useLocale } from "next-intl";
 import { useEffect, useRef, useState, useTransition } from "react";
 import nProgress from "nprogress";
 
+import Flag from "react-world-flags";
 import { Button } from "./ui/button";
 
-import { cn } from "~/lib/utils";
 import { usePathname, useRouter } from "~/i18n/navigation";
+import { cn } from "~/lib/utils";
 
 type Language = {
   code: string;
   name: string;
   flag: string;
+  flagCode: string;
 };
 
 const languages: Language[] = [
-  { code: "en", name: "English", flag: "🇺🇸" },
-  { code: "id", name: "Indonesia", flag: " 🇮🇩" },
+  { code: "en", name: "English", flag: "🇺🇸", flagCode: "GB" },
+  { code: "id", name: "Indonesia", flag: " 🇮🇩", flagCode: "ID" },
   // { code: "es", name: "Español", flag: "🇪🇸" },
   // { code: "fr", name: "Français", flag: "🇫🇷" },
   // { code: "de", name: "Deutsch", flag: "🇩🇪" },
@@ -90,24 +92,42 @@ export default function LanguageDropdown({
       ref={dropdownRef}
       className={cn("relative block w-min text-left", className)}
     >
+      {/* <FlagPack code="US" size="m" />
+      <FlagPack code="CA" size="m" /> */}
+      {/* <span className="flag-icon flag-icon-us" /> */}
       {/* Dropdown trigger button */}
       <Button
         type="button"
-        className="inline-flex w-fit items-center justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+        className="border-shadow-sm flex items-center border border-gray-300 bg-white text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+        // className="items-center justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
         onClick={() => setIsOpen(!isOpen)}
         aria-expanded={isOpen}
         aria-haspopup="true"
       >
-        <span className="flex items-center">
-          <span className="text-base md:mx-2">{selectedLanguage.flag}</span>
-          <span className="hidden md:inline-block">
-            {selectedLanguage.name}
-          </span>
-        </span>
+        {/* <span className="flex items-center gap-2">
+          {/* <pre>{JSON.stringify({ selectedLanguage }, null, 2)}</pre> */}
+        {/* <span>
+
+</span> */}
+        {/* </span> */}
+        <Flag
+          className="inline w-5"
+          code={selectedLanguage.flagCode}
+          fallback={<span>Unknown</span>}
+        />
+        {/* <span className="text-lg">
+          <ReactCountryFlag countryCode={selectedLanguage.flagCode} />
+        </span> */}
+        {/* <Flag
+          className="h-5 w-5"
+          code={selectedLanguage.flagCode}
+          fallback={<span>Unknown</span>}
+        /> */}
+        {selectedLanguage.code.toUpperCase()}
         <motion.span
           animate={{ rotate: isOpen ? 180 : 0 }}
           transition={{ duration: 0.2 }}
-          className="md:ml-2"
+          // className="md:ml-2"
         >
           <ChevronDown className="h-4 w-4" />
         </motion.span>
@@ -125,27 +145,44 @@ export default function LanguageDropdown({
             style={{ transformOrigin: "top" }}
           >
             <div
-              className="max-h-60 w-full overflow-auto py-1"
+              // className="relative w-full py-1"
               style={{ scrollbarWidth: "thin" }}
             >
               {languages.map((language) => (
                 <button
                   key={language.code}
                   className={cn(
-                    "flex w-full items-center justify-between px-4 py-2 text-left text-sm hover:bg-gray-100",
+                    // "bg-white",
+                    "flex w-full items-center justify-between gap-2 overflow-x-visible whitespace-nowrap px-4 py-2 text-right text-sm hover:bg-gray-100",
                     selectedLanguage.code === language.code
-                      ? "bg-gray-50 text-primary"
+                      ? "bg-gray-200 text-primary"
                       : "text-gray-700",
                   )}
                   onClick={() => handleLanguageSelect(language)}
                 >
-                  <span className="whitespace-nowrap">
-                    <span className="mr-2 text-base">{language.flag}</span>
-                    <span className="flex-grow">{language.name}</span>
-                  </span>
-                  {selectedLanguage.code === language.code && (
-                    <Check className="ml-1 h-4 w-4 text-primary" />
-                  )}
+                  {/* <span className="flex gap-2 whitespace-nowrap">
+                   */}
+                  {/* <Flag code={language.code} size={"m"} /> */}
+                  {/* <span className="text-lg leading-none">
+                      <ReactCountryFlag countryCode={language.flagCode} />
+                    </span> */}
+                  {/* <span className="mr-2 text-base">{language.flag}</span> */}
+                  {/* 
+                  </span> */}
+
+                  <div className="flex w-full items-center justify-start gap-2 whitespace-nowrap text-start">
+                    <Flag
+                      className="inline w-5"
+                      code={language.flagCode}
+                      fallback={<span>Unknown</span>}
+                    />
+                    <span className="flex-grow">
+                      {language.code.toUpperCase()}
+                    </span>
+                  </div>
+                  {/* {selectedLanguage.code === language.code && (
+                    <Check className="ml-4 h-4 w-4 text-primary" />
+                  )} */}
                 </button>
               ))}
             </div>

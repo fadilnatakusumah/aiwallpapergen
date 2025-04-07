c2087ab6c248d1ee3b0a3cb543e1f1396cec9654
fadilnatakusumah <fadil.ntksmh@gmail.com>
2025-04-07 10:50:37 +0700

    fix: improve next-sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,32 +1,32 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   // The base URL of your site; ensure this is set correctly in production.
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://aiwallpapergen.com",
-  
+  siteUrl: process.env.NEXTAUTH_URL || "https://aiwallpapergen.com",
+
   // Generate a robots.txt file along with the sitemap.
   generateRobotsTxt: true,
-  
+
   // Maximum URLs per sitemap file.
   sitemapSize: 7000,
-  
+
   // Default change frequency for all pages.
   changefreq: "daily",
-  
+
   // Default priority for all pages.
   priority: 0.7,
-  
+
   // Exclude these routes from the sitemap.
-  exclude: [
-    "/api/*",
-    "/auth/*",
-    "/c/*",
-    "/explore",
-    "/my-collections",
-    "/terms-of-service",
-    "/privacy-policy",
-    "/status",
-  ],
-  
+  // exclude: [
+  //   "/api/*",
+  //   "/auth/*",
+  //   "/c/*",
+  //   "/explore",
+  //   "/my-collections",
+  //   "/terms-of-service",
+  //   "/privacy-policy",
+  //   "/status",
+  // ],
+
   // Manually add key static paths that might not be auto-detected,
   // especially useful if using Next.js App Router.
   additionalPaths: async (config) => {
@@ -35,6 +35,33 @@ const config = {
       { loc: `${config.siteUrl}/contact-us` },
       // Add additional pages if needed.
     ];
+  },
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/api/*",
+          "/auth/*",
+          "/c/*",
+          "/explore/",
+          "/my-collections/",
+          "/terms-of-service/",
+          "/privacy-policy/",
+          "/status/",
+        ],
+      },
+
+      {
+        userAgent: "Applebot",
+        disallow: ["/"],
+      },
+      {
+        userAgent: "Bingbot",
+        disallow: ["/"],
+      },
+    ],
   },
 };
 
